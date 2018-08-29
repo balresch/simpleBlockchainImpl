9849cd3d0b5a87e8c8bfc9a5ed7c766b6cae289a
balresch <balresch@gmail.com>
2018-08-29 14:47:02 +0200

    done, multi-node-operation not tested

--- SimplePythonBlockchain/blockchain.py
+++ SimplePythonBlockchain/blockchain.py
@@ -153,7 +153,7 @@ class Blockchain(object):
             return True
 
         return False
-    
+
 
 
 
@@ -264,6 +264,44 @@ def full_chain():
         "chain": blockchain.chain,
         "length": len(blockchain.chain)
     }
+    return jsonify(response), 200
+
+
+@app.route("/nodes/register", methods=["POST"])
+def register_node():
+    values = request.get_json()
+
+    nodes = values.get("nodes")
+
+    if nodes is None:
+        return "Error: please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        "message": "New nodes have been added",
+        "total_nodes": list(blockchain.nodes)
+    }
+
+    return jsonify(response), 201
+
+@app.route("/nodes/resolve", methods=["GET"])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            "message": "Our chain was replaced",
+            "new_chain": blockchain.chain
+        }
+
+    else:
+        response = {
+            "message": "Our chain is authoritative",
+            "chain": blockchain.chain
+        }
+
     return jsonify(response), 200
 
 if __name__ == "__main__":
